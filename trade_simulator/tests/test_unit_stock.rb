5d2341566a27d82b544f33cd056c3b25fd5dc584
Yoichi Kimura <kimura.yoichi@gmail.com>
2016-01-15 15:05:28 +0900

    test

--- trade_simulator/tests/test_unit_stock.rb
+++
@@ -1,9 +0,0 @@
-require 'test/unit'
-require "./lib/stock"
-
-class Test_unit_stock < Test::Unit::TestCase
-  def stock_test
-    stock = Stock.new(8604,:t, 100)
-    assert_equal(8604,:t, 100)
-  end
-end
